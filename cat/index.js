cf491688e60b9763308af871ca3563024fc6fc42
maxh <booah8@gmail.com>
2017-06-27 17:51:15 -0400

    👍👎

--- cat/index.js
+++ cat/index.js
@@ -30,7 +30,10 @@ module.exports = config => new Promise(function(resolve, reject) {
 		const picked = images[Math.floor(Math.random() * images.length)];
 		fetch(picked.img).then(()=>{
 			if(config.lastmessage) config.sendMessage(config.lastmessage);
-			config.sendMessage({file: {attachment: picked.img}});
+			config.sendMessage({file: {attachment: picked.img}}).then(message=>{
+				message.react("👍");
+				message.react("👎");
+			});
 			resolve("(From " + picked.src + " )");
 		}).catch(reply);
 	};
