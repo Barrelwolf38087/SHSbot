58531713247adfe9fef43af3307fe9426fb0a6b2
maxh <booah8@gmail.com>
2017-06-28 18:46:20 -0400

    Fix help

--- help/index.js
+++ help/index.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 
 module.exports = config => new Promise((resolve, reject)=>{
-	var str = `${config.config.messages.help}`;
+	var str = `\`\`\`${config.config.messages.help}`;
 
 	config.directories.forEach(dir=>{
 
-		str += `\`${config.config.prefix}${dir}: ${config.template(config.configs[dir].description, config.config)}\`\n`;
+		str += `${config.config.prefix}${dir}: ${config.template(config.configs[dir].description, config.config)}\n`;
 	});
+
+	str += "```";
 	console.log("Sending help", str);
 	resolve(str);
 });
