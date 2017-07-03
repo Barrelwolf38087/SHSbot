4790e96769769ab9b7867b5c524a2811694cf7fc
maxh <booah8@gmail.com>
2017-07-03 10:35:08 -0400

    Fix $pict chop

--- pict/chop/index.js
+++ pict/chop/index.js
@@ -7,7 +7,7 @@ module.exports = config => new Promise(function(resolve, reject) {
 		const x2 = parseInt(config.commandArr[2], 10) || x + 50;
 		const y2 = parseInt(config.commandArr[3], 10) || y + 50;
 		console.log("chopping ", x, "x", y, "by", x2, "x", y2);
-		const res = gm(file).chop(x, y, x2, y2).write("temp/done.jpeg", console.log);
+		const res = gm(file).chop(x, y, x2, y2);
 		resolve(res);
 	}), true).then(resolve).catch(reject);
 });
