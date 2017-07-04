49d11c73ff1668717ae34c0f1b1d8e49ca855893
maxh <booah8@gmail.com>
2017-07-04 13:38:50 -0400

    Fix

--- pict/edgey/index.js
+++ pict/edgey/index.js
@@ -3,7 +3,7 @@ const gm = require("gm");
 module.exports = config => new Promise(function(resolve, reject) {
 	require("../../lastimg.js")(config, file=>new Promise(function(resolve) {
 		const num = parseInt(config.commandArr[0], 10) || 100;
-		console.log("blurring by", num);
+		console.log("edgey by", num);
 		const res = gm(file).edge(num);
 		resolve(res);
 	}), true).then(resolve).catch(reject);
