a2b48fe62f34307790c986f668a38f19eb83b033
maxh <booah8@gmail.com>
2017-08-01 17:49:33 -0400

    Fix $coins

--- coins/index.js
+++ coins/index.js
@@ -1,10 +1,10 @@
 module.exports = config => {
 	const user = config.commandArr.join(" ").slice(2, -1);
 	if(/^<@\d+>$/.test(config.commandArr.join(" ")) && user){
-		return Promise.resolve(config.coins[user] || config.config.startCoins);
+		return Promise.resolve("That user has " + (config.coins[user] || config.config.startCoins) + config.config.coin);
 	}else if(config.commandArr.length){
 		return Promise.reject("Hmm, looks like I couldn't find that user. Remember that you need to @-ping them.");
 	}else{
-		return Promise.resolve(config.coins[config.author.id]);
+		return Promise.resolve("You have " + config.coins[config.author.id] + config.config.coin);
 	}
 };
