a4ddeef2553212ed6a02effa415046e55fc2f5ab
maxh <booah8@gmail.com>
2017-08-07 12:23:05 -0400

    No modifying commands that you don't have permission to do

--- chperml/index.js
+++ chperml/index.js
@@ -1,4 +1,10 @@
 module.exports = config => {
 	config.commandArr = config.commandArr.slice(0, 3);
+
+	const targetC = config.configs[config.commandArr[0]];
+	if(targetC && targetC.permissions === "00"){
+		return Promise.reject("You can't modify that command.");
+	}
+
 	return require("../.chperm/index.js")(config);
 };
