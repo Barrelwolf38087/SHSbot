456395fb4c9e6228b4893cd47229b78e2405f725
maxh <booah8@gmail.com>
2017-08-07 12:04:08 -0400

    $help hit msg limit

--- help/index.js
+++ help/index.js
@@ -8,6 +8,22 @@ module.exports = config => new Promise((resolve)=>{
 		str += `\`${config.config.prefix}${dir}\`: ${config.template(config.configs[dir].description, config.config)}\n\n`;
 	});
 
-	str += "";
-	resolve(str);
+	if(str.length <= 2000){
+		console.log("less than 2000!");
+		resolve(str);
+	}else{
+		async function doit (str){//jshint ignore: line
+			if(str.length > 2000){
+				console.log(str.slice(0, 2000).length);
+				await config.sendMessage(str.slice(0, 2000));//jshint ignore: line
+				await doit(str.slice(2000));//jshint ignore: line
+				return;
+			}else{
+				console.log(str.length);
+				await config.sendMessage(str);//jshint ignore: line
+				return;
+			}
+		}
+		doit(str).then(resolve);
+	}
 });
