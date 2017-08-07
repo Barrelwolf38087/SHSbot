ca15809894e5374b13a55c7a080324863155649f
maxh <booah8@gmail.com>
2017-08-07 11:34:17 -0400

    $.chmod takes 2 params

--- chmodl/index.js
+++ chmodl/index.js
@@ -1,4 +1,4 @@
 module.exports = config => {
-	config.commandArr = config.commandArr.slice(0, 1);
+	config.commandArr = config.commandArr.slice(0, 2);
 	return require("../.chmod/index.js")(config);
 };
