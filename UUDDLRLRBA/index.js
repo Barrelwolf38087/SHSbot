6f50f5721fa28b579e92edc29ec0200d2b348aa9
maxh <booah8@gmail.com>
2017-08-12 09:39:12 -0400

    Add code #d8c706bc4f00d

--- UUDDLRLRBA/index.js
+++ UUDDLRLRBA/index.js
@@ -1,4 +1,4 @@
 module.exports = config => {
-	config.privateMessage(":tada: Congradulations! You found an easter egg!");
+	config.privateMessage(":tada: Congradulations! You found an easter egg! (Code #d8c706bc4f00d)");
 	return Promise.resolve("I couldn't find the command \"UUDDLRLRBA\". Try `$help`. (If you don't want your message to be treated as a command, add a \ before it.)");
 };
