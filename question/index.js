abde653054e5b8122f9f28b70dc7354b59c195a1
maxh <booah8@gmail.com>
2017-08-23 13:22:38 -0400

    $question in an embed

--- question/index.js
+++ question/index.js
@@ -1,19 +1,28 @@
 const fetch = require("node-fetch");
-const AsciiTable = require("ascii-table");
-
-const limitStr = (str, limit) => {
-	if(str.length > limit){
-		return str.slice(0, limit - 1) + "…";
-	}
-	return str;
-};
-
-const maxLength = 150;
 
 module.exports = config => new Promise((resolve)=>{
-	fetch("https://api.wolframalpha.com/v2/query?input=" + encodeURIComponent(config.commandArr.join(" ")) + "&appid=" + config.config.wolframKey + "&format=plaintext&ignorecase=true&output=json").then(x=>x.json()).then(obj=>{
-		var str = "https://www.wolframalpha.com/input/?i=" + encodeURIComponent(config.commandArr.join(" ")) + "\n";
-		console.log(obj);
+	fetch("https://api.wolframalpha.com/v2/query?input=" + encodeURIComponent(config.commandArr.join(" ")) + "&appid=" + config.config.wolframKey + "&format=plaintext,img,image&ignorecase=true&output=json").then(x=>x.json()).then(obj=>{
+		var resp = {
+		  "embed": {
+		    "title": "Wolfram | Alpha query",
+		    "description": "Your input: `" + config.commandArr.join(" ") + "`",
+		    "url": "https://www.wolframalpha.com/input/?i" + encodeURIComponent(config.commandArr.join(" ")),
+		    "color": 0,
+		    "timestamp": "2017-08-22T17:59:31.297Z",
+		    "footer": {
+		      "icon_url": "https://cdn.discordapp.com/avatars/327884416674168847/a1ac7baecca695bb0679c6ef86ea636a.webp?size=256",
+		      "text": "SHSbot"
+		    },
+		    "author": {
+		      "name": "Wolfram | Alpha",
+		      "url": "https://www.wolframalpha.com/",
+		      "icon_url": "http://products.wolframalpha.com/images/products/products-wa.png"
+		    },
+		    "fields": [
+		    ]
+		  }
+		};
+		//console.log(JSON.stringify(obj));
 		try{
 			obj.queryresult.pods.forEach(pod=>{
 				try{
@@ -22,22 +31,17 @@ module.exports = config => new Promise((resolve)=>{
 		        }
 		        var name = pod.title;
 		        var plaintext = pod.subpods[0].plaintext;
-		        if(plaintext.includes("|")){
-		            plaintext = (new AsciiTable()).setAlignLeft().removeBorder();
-		            const arr = pod.subpods[0].plaintext.split("\n");
-		            arr.forEach(y=>{
-		                plaintext.addRow(
-		                    ...y.split(/\s+\|\s+/g).map(x=>limitStr(x.trim(), maxLength))
-		                );
-		            });
-		        }
-		        var text = limitStr(plaintext + "", maxLength) || pod.subpods[0].img.src;
-		        str += "**" + name + "**\n" + text/*.replace(/\s+\|\s+/g, "\t")*/ + "\n\n";
+						if(name && plaintext){
+							resp.embed.fields.push({name: name, value: plaintext});
+						}
+						if(pod.subpods && pod.subpods[0] && pod.subpods[0].img && pod.subpods[0].img.src && !resp.embed.image){
+							resp.embed.image = {};
+							resp.embed.image.url = pod.subpods[0].img.src;
+						}
 			   }catch(e){
 					console.error(e, pod);
 				}
 			});
-			str = limitStr(str, 2000);
 			/*var a = [];
 			var i = 200;
 
@@ -46,7 +50,8 @@ module.exports = config => new Promise((resolve)=>{
 			a.forEach(bit=>{
 				config.sendMessage(bit);
 			});*/
-			resolve(str);
+			console.log("\n\n\n\n\n", JSON.stringify(resp));
+			resolve(resp);
 		}catch(e){
 			if(!obj.queryresult.success){
 				var help = "";
