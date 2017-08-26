c4d914359e538c53ddaa7dbe178bce89d08eece6
maxh <booah8@gmail.com>
2017-08-26 10:15:12 -0400

    $help command

--- help/index.js
+++ help/index.js
@@ -1,12 +1,17 @@
-module.exports = config => new Promise((resolve)=>{
-	var str = `${config.config.messages.help}\n`;
+module.exports = config => new Promise((resolve, reject)=>{
+	var str = config.commandArr[0] ? "" : `${config.config.messages.help}\n`;
 
-	config.directories.forEach(dir=>{
-		if(dir[0] === "."){
-			return;
-		}
+	var hasFound = false;
+
+	config.directories.filter(x=>x[0] !== ".").filter(x=>!config.commandArr[0] || config.commandArr[0] === x).forEach(dir=>{
+		hasFound = true;
 		str += `\`${config.config.prefix}${dir}\`: ${config.template(config.configs[dir].description, config.config)}\n\n`;
 	});
+
+	if(!hasFound){
+		return reject("That command was not found!");
+	}
+	str = str.trim();
 
 	if(str.length <= 2000){
 		console.log("less than 2000!");
