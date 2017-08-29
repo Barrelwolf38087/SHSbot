425609e9ca215afd9cd660dae042377f62c302ce
maxh <booah8@gmail.com>
2017-08-29 11:31:50 -0400

    fix $bug

--- bug/index.js
+++ bug/index.js
@@ -1,1 +1,1 @@
-module.exports = ()=>Promise.resolve("You can report a bug on github: https://github.com/maxh65000/discord-bot/issues/new?title=Bug:&body=I+found+a+bug: or private message or ping @maxhogan#1514 .");
+module.exports = ()=>Promise.resolve("You can report a bug on github: https://github.com/SHSbot/SHSbot/issues/new?title=Bug:&body=I+found+a+bug: or private message or ping @maxh#1514 .");
