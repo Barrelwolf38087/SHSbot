ce77276b61c10ac5e8b73ffd7e20826e5c4ee5be
maxh <booah8@gmail.com>
2017-08-29 10:22:21 -0400

    Fix

--- help/index.js
+++ help/index.js
@@ -4,8 +4,12 @@ module.exports = config => new Promise((resolve, reject)=>{
 	var hasFound = false;
 
 	config.directories.filter(x=>x[0] !== ".").filter(x=>!config.commandArr[0] || config.commandArr[0] === x).forEach(dir=>{
-		hasFound = true;
-		str += `\`${config.config.prefix}${dir}\`: ${config.template(config.configs[dir].description, config.config)}\n\n`;
+		try{
+			hasFound = true;
+			str += `\`${config.config.prefix}${dir}\`: ${config.template(config.configs[dir].description, config.config)}\n\n`;
+		}catch(e){
+			console.error(e);
+		}
 	});
 
 	if(!hasFound){
