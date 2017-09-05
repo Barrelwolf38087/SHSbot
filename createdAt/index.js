74e63327e8ac47323de1c96825a09adae01ed898
maxh <booah8@gmail.com>
2017-09-05 13:08:59 -0400

    Pad 0s on minutes & seconds

--- createdAt/index.js
+++ createdAt/index.js
@@ -1,6 +1,7 @@
+const pad2 = str => ("0" + str).slice(-2);
 
 module.exports = config => {
 	var date = config.guilds.get(config.guildId).createdAt;
-	var str = `This server was created on ${date.getMonth()}/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+	var str = `This server was created on ${date.getMonth()}/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}:${pad2(date.getMinutes())}:${pad2(date.getSeconds())}`;
 	return Promise.resolve(str);
 };
