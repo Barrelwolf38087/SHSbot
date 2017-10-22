afa9be3d755917d5ea13752ae49d0ccc9d639f3a
maxh65000 <booah8@gmail.com>
2017-10-22 08:39:41 -0400

    fix

--- ???/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = config=>new Promise(resolve=>{
-	const num = parseInt(config.commandArr[0], 10) || 1;
-	const res = "???(".repeat(num) + "???" + ")".repeat(num);
-	console.log("??? by", num, "is", res);
-	resolve(res);
-});
