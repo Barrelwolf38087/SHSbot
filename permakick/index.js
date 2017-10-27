4cf38401c7c3a7b5f6c1220468e94be9eb71634d
philip.hogan <29708527+maxh65000@users.noreply.github.com>
2017-10-27 17:24:20 -0400

    fix $permakick

--- permakick/index.js
+++ permakick/index.js
@@ -2,10 +2,11 @@ module.exports = config => new Promise((resolve, reject) => {
 	if(!config.commandArr[0]){
 		return reject("You need to specify a user to ban!");
 	}
-	var user = config.searchForUser(config.commandArr[0]);
+	var user = config.searchForUser(config.commandArr.join(" "));
 	if(!user){
 		return reject("I couldn't find that user.");
 	}
 	config.addPermakick(user.user.id);
+  user.kick("Permakicked");
   return resolve("Done!");
 });
