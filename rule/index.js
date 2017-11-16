49e1e15c70d56c23b14984896f48154fdbe36a3b
philip.hogan <programmer5000@programmer5000.com>
2017-11-15 21:00:09 -0500

    rich embed

--- rule/index.js
+++ rule/index.js
@@ -1,6 +1,9 @@
 module.exports = config => {
 	if(config.rules[parseInt(config.commandArr[0])]){
-		return Promise.resolve(config.rules[parseInt(config.commandArr[0])]);
+		return Promise.resolve({embed: {
+      "title": "Rule #" + config.commandArr[0],
+      "description": config.rules[parseInt(config.commandArr[0])]
+    }});
 	}else{
     return Promise.reject("Rule #" + config.commandArr[0] + " not found!");
   }
