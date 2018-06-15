adb67a23af8d5149882c8f1c07b8b0a8311489b0
maxh65000 <booah8@gmail.com>
2018-06-15 12:14:10 -0400

    Do something

--- spam/index.js
+++ spam/index.js
@@ -1,1 +1,1 @@
-module.exports = config => Promise.resolve(config.commandArr.join(" ").repeat(2000).slice(0, 2000));
+module.exports = config => Promise.resolve((config.commandArr.join(" ") || "i\nh").repeat(2000).slice(0, 2000));
