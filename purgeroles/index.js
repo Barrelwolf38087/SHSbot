9384a840483788309ce3481e45a6e7259044f79b
maxh65000 <booah8@gmail.com>
2018-09-01 11:09:34 -0400

    Add a dry run functionality to $purgeroles

--- purgeroles/index.js
+++ purgeroles/index.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 
 module.exports = config => new Promise((resolve) => {
+	let dryRun = false;
+	let report = "";
+	if(config.commandArr[0] === "true"){
+		config.channel.send("Debugging & dry-run enabled!");
+		dryRun = true;
+	}
 	fs.readFile("purgeroles.txt", (err, data) => {
 		if(err) console.error(err);
 		data = data.toString();
@@ -17,13 +23,47 @@ module.exports = config => new Promise((resolve) => {
 				config.sendMessage("Could not get role " + role + " " + e);
 			}
 		}).filter(Boolean);
-		config.channel.guild.members.array().forEach(member => {
+
+		config.channel.guild.roles.forEach(role => {
+			if(role.name.toLowerCase().includes("advisory")){
+				report += `
+Found advisory role ${role.name} (${role.id})`;
+				console.log("it has advisory");
+				roles.push(role);
+			}
+		});
+
+		if(dryRun){
+			report += `
+
+Final Roles:`;
 			roles.forEach(role => {
-				member.removeRole(role).catch(() => {
-					config.sendMessage(`Could not remove role ${role} from ${member.user.tag} (${member.user.id})`);
-					resolve("Done!");
+				report += `
+${role.name} (${role.id})`;
+			});
+		}
+
+		config.channel.guild.members.array().
+			filter(member =>
+				member.roles.some(role => /freshmen|sophomores|juniors|seniors/.test(role.name.toLowerCase()))
+			).
+			forEach(member => {
+				report += `
+
+Member ${member.user.tag}`;
+				roles.forEach(role => {
+					report += `
+Removing role ${role.name} (${role.id})`;
+					if(!dryRun){
+						member.removeRole(role).catch(() => {
+							config.sendMessage(`Could not remove role ${role} from ${member.user.tag} (${member.user.id})`);
+							resolve("Done!");
+						});
+					}
 				});
 			});
-		});
+		if(dryRun){
+			config.channel.send(report);
+		}
 	});
 });
