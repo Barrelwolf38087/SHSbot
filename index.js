7c3c92f78a34cf8e8b0bfd0b8ccb95e2ae769e47
maxh65000 <booah8@gmail.com>
2018-10-07 20:00:38 -0400

    Improve logging

--- index.js
+++ index.js
@@ -137,6 +137,7 @@ client.on("ready", () => {
 	});
 	console.log("Ready!");
 	const g = client.guilds.get(config.guildId);
+	console.log("Getting", config.guildId, "from", config.guilds.array(), "got", g);
 	g.fetchInvites().then(newInvites=>{
 		newInvites.forEach(invite => {
 			invites[invite.code] = invite.uses;
